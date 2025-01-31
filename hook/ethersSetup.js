5cd844f7b2a0ef99bd60559c6cf937bac2a8da0c
callmeweirdo <ogbandav102@gmail.com>
2025-01-31 19:02:59 +0100

    working on the react native side of the project to make sure everything works perfect

--- hook/ethersSetup.js
+++ hook/ethersSetup.js
@@ -1,9 +1,14 @@
 import { ethers } from "ethers";
-import { portfolioManagerABI } from "../contract/abi.json";
+import { portfolioManagerABI } from "../contract/PortfolioManager.json";
+import { EducationABI } from "../contract/Education.json";
+import { ProjectABI } from "../contract/Projects.json";
+import { ExperienceABI } from "../contract/Experience.json";
+// import { portfolioManagerABI } from "../contract/PortfolioManager.json";
+
+const portfolioManagerAddress  = "dcfgvhbjnk";
 
 // Use Hardhat node (or update to Infura/Alchemy for testing/Mainet)
 
 const provider = new ethers.JsonRpcProvider("http://localhost:8645");
 
 // Smart contract details
-
--- hook/walletConnect.js
+++ hook/walletConnect.js
@@ -6,57 +6,42 @@ import { ethers } from "ethers";
 let connector;
 
 export const connectWallet = async () => {
-    if (!connector) {
-        connector = new WalletConnect({
-            bridge: "https://bridge.walletconnect.org" // Corrected the bridge URL
-        });
-    }
-
-    if (!connector.connected) {
-        // Corrected the typo: 'creatSession' should be 'createSession'
-        await connector.createSession();
-
-        // Display QR code for wallet connection on web
-        if (Platform.OS === "web") {
-            QRCodeModal.open(connector.uri, () => {
-                console.log("QR Code Modal closed");
-            });
-        } else {
-            // For mobile, use deep linking to open MetaMask
-            const metamaskUrl = `https://metamask.app.link/wc?uri=${encodeURIComponent(connector.uri)}`;
-            Linking.openURL(metamaskUrl);
-        }
-    }
-
-    return new Promise((resolve, reject) => {
-        connector.on("connect", (error, payload) => {
-            if (error) {
-                reject(error);
-            } else {
-                QRCodeModal.close();
-                const { accounts } = payload.params[0];
-                resolve(accounts[0]);  // Return the connected wallet address
-            }
-        });
+  if (!connector) {
+    connector = new WalletConnect({
+      bridge: "https://bridge.walletconnect.org", 
     });
-};
+  }
 
-// This function gets the signer (wallet instance) connected to the local Hardhat network
-export const getSigner = async () => {
-    if (!connector || !connector.connected) {
-        throw new Error("Wallet not connected");
+  if (!connector.connected) {
+    await connector.createSession();
+    if (Platform.OS === "web") {
+      QRCodeModal.open(connector.uri, () => {
+        console.log("QR Code Modal closed");
+      });
+    } else {
+      const metamaskUrl = `https://metamask.app.link/wc?uri=${encodeURIComponent(connector.uri)}`;
+      Linking.openURL(metamaskUrl);
     }
+  }
 
-    // Connect to local Hardhat network
-    const provider = new ethers.JsonRpcProvider("http://localhost:8545"); // Local Hardhat network
-    const signer = new ethers.Wallet(connector.accounts[0], provider);
-    return signer;
+  return new Promise((resolve, reject) => {
+    connector.on("connect", (error, payload) => {
+      if (error) {
+        reject(error);
+      } else {
+        QRCodeModal.close();
+        const { accounts } = payload.params[0];
+        resolve(accounts[0]);
+      }
+    });
+  });
 };
 
-// This function disconnects the wallet
 export const disconnectWallet = async () => {
-    if (connector) {
-        await connector.killSession();
-        return null; // Return null to indicate that the wallet is disconnected
-    }
+  if (connector) {
+    await connector.killSession();
+    connector = null;
+  }
 };
+
+export { connector };
