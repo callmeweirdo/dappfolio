8c12a5fc9299db65f68d20a5ef0876f5721bd09e
callmeweirdo <ogbandav102@gmail.com>
2025-02-11 20:42:18 +0100

    now working on this project from my ubuntu machine and now am currently updating the useContract script

--- hook/useContract.js
+++ hook/useContract.js
@@ -8,7 +8,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MetaMaskSDK } from "@metamask/sdk-react-native";
 
 const metamask = new MetaMaskSDK();
-const PortfolioManagerAddress = "YOUR_CONTRACT_ADDRESS";
+const PortfolioManagerAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 export const useContracts = () => {
     const [portfolioManagerContract, setPortfolioManagerContract] = useState(null);
@@ -22,15 +22,22 @@ export const useContracts = () => {
     const [experienceDetails, setExperienceDetails] = useState([]);
 
     const [account, setAccount] = useState(null);
-    const [connected, setConnected] = useState(null);
+    const [connected, setConnected] = useState(false);
     const [chainId, setChainId] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
 
     useEffect(() => {
         const initializeContracts = async () => {
+            setLoading(true);
             try {
-                const provider = metamask.getProvider();
+                const provider = await metamask.getProvider();
+
+                if (!provider) {
+                    throw new Error("MetaMask provider not found") // handles the case where provider is not available
+                }
+
                 const web3Provider = new ethers.providers.web3Provider(provider);
                 const signer = web3Provider.getSigner();
                 const address = await signer.getAddress();
